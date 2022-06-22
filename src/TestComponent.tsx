2281f14ec3f9557bed95cffd506229a9ac16bc1b
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-06-22 14:43:10 +0900

    React Hooks useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   text: string;
 }
-
+interface UserData {
+  id: number;
+  name: string;
+}
 const TestComponent: React.FC<Props> = (props) => {
+  // React Hooks useState
+  const [count, setCount] = useState<number | null>(null);
+
+  // const [count, setCount] = useState(0);
+  // 初期値に0が入っているので、countの型はnumberで、
+  // setCountの引数の型もnumberに自動的になっている
+
+  const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
     </div>
   );
 };
